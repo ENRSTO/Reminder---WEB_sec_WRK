7c71616fe45ec553a6d1e71a8811c6dd432212f7
enrsto <'enrico.stocchetti@sanmarcoinformatica.it'>
2023-11-19 18:45:13 +0100

    ex-11

--- src/main/java/com/example/demo/model/Cliente.java
+++ src/main/java/com/example/demo/model/Cliente.java
@@ -10,6 +10,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+
+// ex - 11
+
+
 @Entity
 @Table(name = "clienti")
 public class Cliente {
