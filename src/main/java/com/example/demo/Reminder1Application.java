0a2e051b513add722cfacc514217a5868e493914
enrsto <'enrico.stocchetti@sanmarcoinformatica.it'>
2023-11-21 14:15:17 +0100

    Buck Rogers 1 commit

--- src/main/java/com/example/demo/Reminder1Application.java
+++ src/main/java/com/example/demo/Reminder1Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Reminder1Application {
     //main STock + main zeb
+    // buck Rogers
 	public static void main(String[] args) {
 		SpringApplication.run(Reminder1Application.class, args);
 	}
