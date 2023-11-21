019ad44d0b9c1b77322c93412c803b7224e8a7af
enrsto <'enrico.stocchetti@sanmarcoinformatica.it'>
2023-11-21 13:26:20 +0100

    ex-53-zeb commitato

--- src/main/java/com/example/demo/Reminder1Application.java
+++ src/main/java/com/example/demo/Reminder1Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Reminder1Application {
-
+    // main zeb
 	public static void main(String[] args) {
 		SpringApplication.run(Reminder1Application.class, args);
 	}
